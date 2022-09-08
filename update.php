34fa9e0d8e205410763c309b323143ec6c631457
Wasim A Pinjari <95426296+wasimapinjari@users.noreply.github.com>
2022-09-08 12:29:06 +0530

    Update update.php

--- update.php
+++ update.php
@@ -1,11 +1,11 @@
 <?php
 
-    /*-------------------------------------------------------------------------------
+    /*
 
     ajax-crud: update.php
     Licensed under MIT (https://github.com/wasimapinjari/ajax-crud/blob/main/LICENSE)
 
-    -------------------------------------------------------------------------------*/
+    */
     
     include './database.php';
 
