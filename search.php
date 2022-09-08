cc75e3b9216b34780f00a3bef95569826c5ddd7b
Wasim A Pinjari <95426296+wasimapinjari@users.noreply.github.com>
2022-09-08 12:29:58 +0530

    Update search.php

--- search.php
+++ search.php
@@ -1,11 +1,11 @@
 <?php
 
-    /*-------------------------------------------------------------------------------
+    /*
 
     ajax-crud: search.php
     Licensed under MIT (https://github.com/wasimapinjari/ajax-crud/blob/main/LICENSE)
 
-    -------------------------------------------------------------------------------*/
+    */
     
     include './database.php';
 
